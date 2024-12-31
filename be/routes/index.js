529f4f2653d8772685fffb29f8d040474b47a307
duonghoccodefullstack <dbui99620.com>
2025-01-01 00:11:01 +0700

    EC-SP1 add routes manager

--- be/routes/index.js
+++ be/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import userRouter from './ver1/auth/authRoutes.js'
+import manageRouter from "./ver1/manager/managerRoutes.js";
 
 
 
 const RootRouter = express.Router();
 // endpoints : http://{url}/api/v1 
 RootRouter.use('/api/v1/auth', userRouter)
-
+RootRouter.use('/api/v1/manager', manageRouter)
 export default RootRouter
