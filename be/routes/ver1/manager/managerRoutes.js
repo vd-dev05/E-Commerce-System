60e96b0ef6d52cad6e80960b620c498ea196dd22
duonghoccodefullstack <dbui99620.com>
2025-01-01 00:05:30 +0700

    EC-SP1 add routes create product

---
+++ be/routes/ver1/manager/managerRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import ManagerProductController from "../../../controllers/manager/products/productController.js";
+
+const manageRouter = express.Router();
+
+manageRouter.post('/create',ManagerProductController.createProducts)
+export default manageRouter
